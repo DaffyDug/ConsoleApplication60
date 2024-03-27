a1da54bbf626b4a585adddc9775c6ec50bc690d8
Александр Александр <stripalol@inbox.ru>
2024-03-27 16:52:34 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication60/ConsoleApplication60.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+double SQRT(double num)
+{
+	for (double i = 0; i < num; i += 0.00001)
+	{
+		if (i * i >= num)
+		{
+			return i;
+		}
+	}
+	return 0;
+}
+double da(double x, double y)
+{
+	double Y = abs(x - y);
+	Y /= y;
+	return Y;
+}
+
+int main()
+{
+	cout << SQRT(103) << endl;
+	cout << SQRT(0.47368) << endl;
+	cout << "delta=" << da(SQRT(103), 0.47368) << '\n';
+}
